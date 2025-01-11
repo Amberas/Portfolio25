3a7bf3d955b4a545eb73bfcdf4c1416647ef7b2e
Amberas <amber.aspeslagh@gmail.com>
2025-01-11 19:32:06 +0100

    seo optimalisation titles & metatext

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
 import "./globals.css";
 
 
-export const metadata = {
-  title: "Amber Aspeslagh | Product Developer, Designer, Researcher Portfolio",
-  description: "Discover Amber Aspeslagh's expertise in product development. I design innovative solutions, develop cutting-edge products, and deliver exceptional results.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">  
     <head>
     <link rel="preconnect" href="https://stijndv.com" />
-    <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" /> <title>Amber Aspeslagh | Product Developer Portfolio</title>
+        <meta name="description" content="Discover Amber Aspeslagh's expertise in product development. I design innovative solutions, develop cutting-edge products, and deliver exceptional results." />
+        <meta name="keywords" content="Amber Aspeslagh, Product Developer, Portfolio, Product Design, Development" />
+        <meta name="author" content="Amber Aspeslagh" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://amberaspeslagh.be" />
   </head>
         {children}
     </html>
