65a63d07ce0367adbbd8a22551d6104c113b3dff
Amberas <amber.aspeslagh@gmail.com>
2025-01-11 20:12:40 +0100

    add Google Analytics

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import Header from "./components/js/Header";
 export default function Home() {
   return (
     <body>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PK7M3W83"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
     <Header/>
     </body>
   );
