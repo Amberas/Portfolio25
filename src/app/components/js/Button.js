98a19cf9a18b2bcdcdb56b8ac87cc611ec186ef6
Amberas <amber.aspeslagh@gmail.com>
2025-01-12 19:34:42 +0100

    add mailto

--- src/app/components/js/Button.js
+++ src/app/components/js/Button.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import styles from "./../css/button.css";
 
 export default function Button() {
+    const email = "amber.aspeslagh@gmail.com";
+    const subject = "I want to work togehter";
+    const body = "I wanted to reach out to discuss...";
+
     return (
-        <a className="button">Let's work together</a>
+        <a href={`mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`} className="button">Let's work together</a>
   );
 }
