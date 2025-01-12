ff96481ea79e7d445ddb4ee970c29b5627ae9072
Amberas <amber.aspeslagh@gmail.com>
2025-01-12 19:37:43 +0100

    change email text

--- src/app/components/js/Button.js
+++ src/app/components/js/Button.js
@@ -3,8 +3,7 @@ import styles from "./../css/button.css";
 
 export default function Button() {
     const email = "amber.aspeslagh@gmail.com";
-    const subject = "I want to work togehter";
-    const body = "I wanted to reach out to discuss...";
+    const subject = "Let's build something together";
 
     return (
         <a href={`mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`} className="button">Let's work together</a>
